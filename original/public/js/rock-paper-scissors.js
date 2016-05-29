fa45f0a0625070b0b7f73fcf054c96736a662637
pokohide <hyde31415@gmail.com>
2016-05-29 16:02:30 +0900

    HPを削ってjs上で戦闘は可能に

--- original/public/js/rock-paper-scissors.js
+++ original/public/js/rock-paper-scissors.js
@@ -10,9 +10,13 @@ jQuery(function($){
   var draw = 0;
   var winning_streak = 0;
 
-  setTimeout(function() {
-    $('#title').text('rock-paper-scissors');
-  }, 2000);
+  const random_var = Math.floor(Math.random()*4)
+  const ENEMY_NAME = ['キマイラ', 'メデューサ', 'ワイバーン', 'ウルフ'][random_var]
+  var ENEMY_HP = [120, 100, 212, 180][random_var]
+  const ENEMY_ATTACK = [21, 14, 41, 31][random_var]
+  var MY_HP = 100 + random_var * 14
+
+  console.log(ENEMY_HP)
 
   $(function() {
       $.ajax({
@@ -76,22 +80,55 @@ jQuery(function($){
     var result;
     if (myHand === opponentHand) {
       result = RESULT_CODE.DRAW;
-      draw += 1;
-      winning_streak = 0;
+      when_draw();
     } else if ((myHand === HAND_TYPE[0] && opponentHand === HAND_TYPE[1]) ||
                (myHand === HAND_TYPE[1] && opponentHand === HAND_TYPE[2]) || 
                (myHand === HAND_TYPE[2] && opponentHand === HAND_TYPE[0])) {
       result = RESULT_CODE.WIN;
-      your_win += 1;
-      winning_streak += 1;
+      when_win();
     }else {
       result = RESULT_CODE.LOSE;
-      your_lose += 1;
-      winning_streak = 0;
+      when_lose();
     }
     html += '<td>' + RESULT_MESSAGE[result] + '</td>';
     const $tr = $('<tr>').html(html);
     $('#history').append($tr);
+    console.log('enemy HP is ' + ENEMY_HP);
+    console.log('my HP is ' + MY_HP);
     return result;
   }
+
+
+  // 負けた場合の処理
+  function when_lose() {
+      your_lose += 1;
+      winning_streak = 0;
+      MY_HP -= ENEMY_ATTACK
+      if(MY_HP <= 0) {
+        gameover(false)
+      }
+  }
+  // 買った場合の処理
+  function when_win() {
+      your_win += 1;
+      winning_streak += 1;
+      ENEMY_HP -= 10 + winning_streak*5
+      if(ENEMY_HP <= 0) {
+        gameover(true)
+      }
+  }
+  // ドローの場合の処理
+  function when_draw() {
+    draw += 1;
+    winning_streak = 0;
+  }
+
+  function gameover(if_win) {
+    if(if_win) {
+      alert('win!');
+    } else {
+      alert('lose!');
+    }
+  }
+
 });
